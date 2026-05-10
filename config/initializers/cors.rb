5109e76aebbf8d9bc6ad0351058d7685def6f095
Ankit Raj <ankit.raj@clinkrewards.com>
2026-05-10 22:41:07 +0530

    Added cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,20 @@
 # Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
 #
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# Cross-Origin Resource Sharing — allow the frontend (local + Vercel) to hit
+# this API. Read more: https://github.com/cyu/rack-cors
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins(
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "https://competitor-watch-ten.vercel.app"
+    )
+
+    resource "*",
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             expose:  %w[X-Pilot-Restaurant-Id],
+             max_age: 600
+  end
+end
